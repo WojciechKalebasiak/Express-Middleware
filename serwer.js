9a624858e35c750c9ce38223b1b4389efe992dae
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-05-06 03:41:45 +0200

    Fix middleware

--- serwer.js
+++ serwer.js
@@ -4,28 +4,12 @@ app.use('/store',function (req, res, next) {
     console.log('You will be on store soon');
     next(); 
 });
-app.get('/store', function(req,rest,next) {
+app.get('/store', function(req,res,next) {
     res.send('Welcome in store!');
 });
 app.get('/', function (req,res,next) {
    console.log('GET Request for home recieved');
    res.send('Hello GET!');
-});
-app.post('/', function (req,res) {
-    console.log('POST Request recieved')
-    res.send('Hello POST!');
-});
-app.delete('/del_user',function (req,res) {
-   console.log('DELETE user request recieved');
-   res.send('Hello delete!'); 
-});
-app.get('/list_user', function (req,res) {
-    console.log('Request GET for list user recieved');
-    res.send('Here will be user list');
-});
-app.get('/ab*cd', function (req,res) {
-   console.log('Request GET for /ab*cd recieved');
-   res.send("Pattern match"); 
 });
 app.listen(3000);
 app.use(function(req,res,next) {
